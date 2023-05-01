14f30cd9424c8573be3647dc154ed2b3f115d1fc
Carlos Nardi <nardi.cem@gmail.com>
2023-05-01 13:22:21 -0300

    Added speed converter to other tools page.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'converter/index', as: 'index'
   get 'pages/other_tools', as: 'other_tools'
   get 'temperatures/index', as: 'temp'
+  get 'speeds/index', as: 'speed'
 
   match 'converter', to: 'converter#index', via: [:get, :post]
 
