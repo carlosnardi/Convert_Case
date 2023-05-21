3ca19e0a218c9869d14f936b256031b89d8e632b
Carlos Nardi <nardi.cem@gmail.com>
2023-05-21 20:42:48 -0300

    Update about and contact page. Create a new home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # get 'pages/home', as: 'home'
+  get 'pages/home', as: 'home'
   get 'pages/about', as: 'about'
   get 'pages/contact', as: 'contact'
   get 'converter/index', as: 'index'
